f75dd1df07ec56b4df53ce4e2ccefa54b9723070
CameronJ0101 <cameronj010@gmail.com>
2025-10-07 19:28:46 -0500

    BookDetailPage Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
-export default function App() {
-  return <></>;
+import React from "react";
+import BookDetailPage from "./BookDetailPage";
+
+function App() {
+  return (
+    <div>
+      <h1>Book Info</h1>
+      <BookDetailPage id="1" />
+    </div>
+  );
 }
+
+export default App;
