e141988b16e9b3dd3f2c36ee128a2b59fe6c4a7d
AirJordan88 <sturosair@gmail.com>
2025-10-07 22:29:10 -0400

    feat: created BooksPage and updated router on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./catalognav/Layout";
 import BookDetailPage from "./Book_Details/BookDetailPage";
 import Register from "./auth/Register";
+import BooksPage from "./catalognav/BooksPage";
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route index element={<BookDetailPage />} />
-
-        <Route path="/books" element={<BookDetailPage />} />
+        <Route index element={<BooksPage />} />
+        <Route path="/books" element={<BooksPage />} />
+        <Route path="/books/:id" element={<BookDetailPage />} />
         <Route path="/register" element={<Register />} />
       </Route>
     </Routes>
